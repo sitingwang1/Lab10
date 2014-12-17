1bc9b40fac00656f780014a699530d8e41d6a474
sitingwang1 <siting.wang1@marist.edu>
2014-12-16 20:26:56 -0500

    Updata Lab10

--- lab10.sql
+++ lab10.sql
@@ -1,61 +1,6 @@
 ﻿--Lab 10
 --Siting Wang
 
-DROP TABLE IF EXISTS Courses;
-create table Courses (
-    num      integer not null,
-    name     text    not null,
-    credits  integer not null,
-  primary key (num)
-);
-
-
-insert into Courses(num, name, credits)
-values (499, 'CS/ITS Capping', 3 );
-
-insert into Courses(num, name, credits)
-values (308, 'Database Systems', 4 );
-
-insert into Courses(num, name, credits)
-values (221, 'Software Development Two', 4 );
-
-insert into Courses(num, name, credits)
-values (220, 'Software Development One', 4 );
-
-insert into Courses(num, name, credits)
-values (120, 'Introduction to Programming', 4);
-
-select * 
-from courses
-order by num ASC;
-
-
--- Courses and their prerequisites
-create table Prerequisites (
-    courseNum integer not null references Courses(num),
-    preReqNum integer not null references Courses(num),
-  primary key (courseNum, preReqNum)
-);
-
-insert into Prerequisites(courseNum, preReqNum)
-values (499, 308);
-
-insert into Prerequisites(courseNum, preReqNum)
-values (499, 221);
-
-insert into Prerequisites(courseNum, preReqNum)
-values (308, 120);
-
-insert into Prerequisites(courseNum, preReqNum)
-values (221, 220);
-
-insert into Prerequisites(courseNum, preReqNum)
-values (220, 120);
-
-select *
-from Prerequisites
-order by courseNum DESC;
-
 --Q1
 CREATE OR REPLACE FUNCTION PreReqFor(int, REFCURSOR)
 RETURNS REFCURSOR AS
@@ -66,11 +11,11 @@ DECLARE
 BEGIN 
 	OPEN RESULTSET FOR 
 	SELECT num,name,credits
-	FROM Courses c
-	WHERE num in (select p.PreReqNum
-		      from Prerequisites p
-		      where coursr_num = p.courseNum);
-		      return resultset;
+	FROM Courses
+	Join Prerequisities
+	ON Courses.num=prerequisities.preReqNum
+	WHERE Prerequisites.courseNum=course;
+	return resultset;
 END;
 $$
 language plpgsql;
@@ -88,10 +33,10 @@ BEGIN
 	OPEN RESULTSET FOR 
 	SELECT num,name,credits
 	FROM Courses 
-	WHERE num in (select p.courseNum
-		      from Prerequisites p
-		      where course = p.PreReqNum);
-		      return resultset;
+	JOIN Prerequisites
+	ON Courses.num=Prerequisites.courseNum
+	WHERE Prerequisites.preReqNum=course;
+	return resultset;
 END;
 $$
 language plpgsql;
